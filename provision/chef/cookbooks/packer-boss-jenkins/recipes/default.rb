97c9df5fe908c057631c28294ade0a03fb879ae7
Tom Scanlan <tscanlan@vmware.com>
2015-10-27 16:31:41 -0400

    updates for builder role
    - run builds using docker workflow
    - packer builder as a container
    - update roles to support
    - fix #7.  test times down to 18 mins for headless, desktop, docker provision test converge against
    most similar base box

--- provision/chef/cookbooks/packer-boss-jenkins/recipes/default.rb
+++ provision/chef/cookbooks/packer-boss-jenkins/recipes/default.rb
@@ -3,6 +3,7 @@ include_recipe 'jenkins::master'
 jenkins_user node['jenkins-master']['admin_username'] do
   password node['jenkins-master']['admin_password']
   full_name 'Jenkins admin user'
+  email node['jenkins-master']['admin_email']
   public_keys [node['jenkins-master']['admin_public_key']]
 end
 
@@ -20,7 +21,7 @@ jenkins_script 'configure email' do
   action :nothing
 end
 
-node['jenkins-master']['users'].each do |user, hash|
+node['jenkins-master']['users'].to_a.each do |user, hash|
   jenkins_user user do
     full_name hash['full_name']
     email hash['email']
@@ -29,25 +30,30 @@ node['jenkins-master']['users'].each do |user, hash|
   end
 end
 
-node['jenkins-master']['credentials'].each do |user, hash|
+node.run_state['jenkins_private_key'] =
+  node['jenkins-master']['admin_private_key']
+
+node['jenkins-master']['credentials'].to_a.each do |user, hash|
   if hash['password']
-    jenkins_password_credentials user do
+    jenkins_password_credentials user.dup do
+      action :create
       id hash['id']
-      description "password #{hash['full_name']}, #{hash['email']}"
+      description hash['description']
       password hash['password']
     end
   end
 
   if hash['private_key']
-    jenkins_private_key_credentials user do
+    jenkins_private_key_credentials user.dup do
+      action :create
       id hash['id']
-      description "private keys #{hash['full_name']}, #{hash['email']}"
+      description hash['description']
       private_key hash['private_key']
     end
   end
 end
 
-node['jenkins-master']['plugins'].each do |plugin|
+node['jenkins-master']['plugins'].to_a.each do |plugin|
   plugin, version = plugin.split('=')
   jenkins_plugin plugin do
     version version if version
