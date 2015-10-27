f27a2d467de6da937309f7a4aa46b69a8c6ac9e6
Tom Scanlan <tscanlan@vmware.com>
2015-10-27 15:51:21 -0400

    add sonar build as a template, pull it into the main builder, switch to building in containers

--- provision/chef/cookbooks/jenkins-job/recipes/default.rb
+++ provision/chef/cookbooks/jenkins-job/recipes/default.rb
@@ -23,3 +23,20 @@ node['jenkins-job']['job-files'].each do |project, source_file|
     action :create
   end
 end
+
+node['jenkins-job']['job-templates'].each do |project, source_file|
+  xml_filename = "#{node['jenkins-job']['job-dir']}/#{project}"
+
+  template xml_filename do
+    source source_file
+    owner node['jenkins']['user']
+    group node['jenkins']['group']
+    mode '0755'
+    action :create
+  end
+
+  jenkins_job project do
+    config xml_filename
+    action :create
+  end
+end
