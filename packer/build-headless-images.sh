86b912605fc31ffa2a80475518aa65f7638b13c0
Tom Scanlan <tscanlan@vmware.com>
2015-12-04 22:30:42 -0500

    fixes for build

--- packer/build-headless-images.sh
+++ packer/build-headless-images.sh
@@ -3,6 +3,7 @@
 set -e
 
 org=tompscanlan
+export 'PACKER_LOG=1'
 #./packer/build-iso-to-vm.sh
 
 #packer build -force -var "version=$version" packer/headless-stage-2.json
@@ -16,7 +17,18 @@ fi
 if test ! -f "${org}-ubuntu-headless-${version}.tar"; then
   packer build -only ubuntu-docker -var "org=${org}" -var "version=$version" packer/headless.json
 fi
-
 if  test ! -f "${org}-headless-ubuntu-virtualbox-${version}.box"; then
   packer build -force -only ubuntu-virtualbox -var "org=${org}" -var "version=$version" packer/headless.json
+fi
+if  test ! -f "${org}-headless-ubuntu-vmware-${version}.box"; then
+  packer build -force -only ubuntu-vmware -var "org=${org}" -var "version=$version" packer/headless.json
+fi
+if  test ! -f "${org}-headless-centos-vmware-${version}.box"; then
+  packer build -force -only centos-vmware -var "org=${org}" -var "version=$version" packer/headless.json
+fi
+if  test ! -f "${org}-headless-ubuntu-virtualbox-${version}.box"; then
+  packer build -force -only ubuntu-virtualbox -var "org=${org}" -var "version=$version" packer/headless.json
+fi
+if  test ! -f "${org}-headless-centos-virtualbox-${version}.box"; then
+  packer build -force -only centos-virtualbox -var "org=${org}" -var "version=$version" packer/headless.json
 fi
