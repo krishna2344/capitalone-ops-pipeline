b67ce4279523f6a8d65964be536ae3357ad29863
dhg777 <keith.gasser@capitalone.com>
2015-12-08 17:45:21 -0500

    Add no_proxy environment variable and cookbook attribute

--- provision/chef/cookbooks/chef-solo-proxy/metadata.rb
+++ provision/chef/cookbooks/chef-solo-proxy/metadata.rb
@@ -1,14 +1,18 @@
 name             "chef-solo-proxy"
 maintainer       "Ryan Wild"
 license          "MIT"
-description      "Sets up a proxy environment varriable for chef solo"
+description      "Sets up a proxy environment variable for chef solo"
 version          "0.0.1"
 
 attribute "chef_solo_proxy/http_proxy",
-  :description => "Sets an environment varriable for the http proxy",
+  :description => "Sets an environment variable for the http proxy",
   :default => nil
 
 attribute "chef_solo_proxy/https_proxy",
-  :description => "Sets an environment varriable for the https secure proxy",
+  :description => "Sets an environment variable for the https secure proxy",
+  :default => nil
+
+attribute "chef_solo_proxy/no_proxy",
+  :description => "Sets an environment variable for addresses which aren't proxied",
   :default => nil
 
--- provision/chef/cookbooks/chef-solo-proxy/recipes/default.rb
+++ provision/chef/cookbooks/chef-solo-proxy/recipes/default.rb
@@ -5,3 +5,7 @@ end
 if node['chef_solo_proxy']['https_proxy']
     ENV['https_proxy'] = node['chef_solo_proxy']['https_proxy']
 end
+
+if node['chef_solo_proxy']['no_proxy']
+	    ENV['no_proxy'] = node['chef_solo_proxy']['no_proxy']
+end
