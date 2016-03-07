827801789b8f821d05266ce70c2155b2ca1692c5
Jason Riddle <Jason.Riddle@capitalone.com>
2016-03-07 10:36:46 -0500

    Add header to rb, sh, yml, Vagrantfile, and Berksfile

--- packer/scripts/alpine_docker_clean.sh
+++ packer/scripts/alpine_docker_clean.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+#
+# Ops-Pipeline - Templates for automating the production and consumption of images
+# and containers.
+#
+# Copyright 2016 Capital One Services, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -x
 
@@ -10,4 +28,3 @@ set -x
 apk del python-dev fakeroot pax-utils libattr attr tar abuild binutils-libs binutils libgomp pkgconf pkgconfig libgcc gmp mpfr3 mpc1 libstdc++ gcc make patch musl-dbg libc6-compat musl-dev libc-dev fortify-headers g++ build-base expat pcre git squashfs-tools file bzip2 libbz2 libcap cdrkit acct lddtree mkinitfs mtools alpine-sdk libffi gdbm ncurses-libs readline sqlite-libs py-pip
 apk -v cache clean
 du -sh /*
-
